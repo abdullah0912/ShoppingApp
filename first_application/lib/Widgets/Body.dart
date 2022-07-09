6a5b0a30dd5c7424a9c042f74fede7bd59a50f56
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2022-07-09 16:15:22 +0300

    ShoppingApp

--- first_application/lib/Widgets/Body.dart
+++ first_application/lib/Widgets/Body.dart
@@ -8,14 +8,36 @@ class MyBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: const Image(
-          image: AssetImage(
-
-            "assets/images/shop.png",
-          ),
-        ),
+      child: GridView.count(
+  primary: false,
+  padding: const EdgeInsets.all(20),
+  crossAxisSpacing: 10,
+  mainAxisSpacing: 10,
+  crossAxisCount: 2,
+  children: <Widget>[
+    Container(
+      color: Colors.teal[100],
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5)
       ),
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.teal[200],
+      child: const Text('Heed not the rabble'),
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.teal[300],
+      child: const Text('Sound of screams but the'),
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.teal[400],
+      child: const Text('Who scream'),
+    ),
+  ],
+)
     );
   }
 }
--- first_application/lib/Widgets/BottomNavBar.dart
+++ first_application/lib/Widgets/BottomNavBar.dart
@@ -1,31 +1,44 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
-class MyBottomNavBar extends StatelessWidget {
+class MyBottomNavBar extends StatefulWidget {
   const MyBottomNavBar({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyBottomNavBar> createState() => _MyBottomNavBarState();
+}
+
+class _MyBottomNavBarState extends State<MyBottomNavBar> {
+  int currentindex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: currentindex,
+      onTap: (index) => setState(() =>currentindex= index),
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(
             Icons.home,
           ),
           label: 'Home',
+          backgroundColor: Colors.blue,
         ),
         BottomNavigationBarItem(
           icon: Icon(
             Icons.shopify_sharp,
           ),
           label: 'Store',
+          backgroundColor: Colors.orange,
         ),
         BottomNavigationBarItem(
           icon: Icon(
             Icons.person,
           ),
           label: 'Profile',
+          backgroundColor: Colors.green,
         ),
       ],
       selectedItemColor: Colors.blue,
